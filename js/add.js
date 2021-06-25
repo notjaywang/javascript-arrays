2c6153ef562500a194f91093f28d0d7978d3d3fd
Jay Wang <jay33015@hotmail.com>
2021-06-25 03:17:07 +0000

    Add index CSS JS

---
+++ js/add.js
@@ -0,0 +1,4 @@
+const instruments = ['piano', 'drums', 'trumpet'];
+instruments.push( "guitar", "violin", "triangle" );
+instruments.unshift ( "cowbell", "tuba"  );
+
---
+++ js/remove.js
@@ -0,0 +1,6 @@
+const numbers= [ 100, 200, 300, 400, 500 ];
+
+numbers.pop();
+numbers.shift();
+
+console.log( numbers );
